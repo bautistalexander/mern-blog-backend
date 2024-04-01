dc187a7ff19ba890866c97bf65552ce54c4dd4f7
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-04-01 19:35:35 -0400

    Se realizó la ruta para cerrar sesión de usuario

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -68,3 +68,12 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signout = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token').status(200).json({ message: 'El usuario a cerrado sesión' });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
