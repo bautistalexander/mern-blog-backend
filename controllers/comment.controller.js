1ce32850024593fef6942e8f8fff35fc3e44759f
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-04-12 17:00:11 -0400

    Se adicionó la ruta para eliminar un comentario

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -67,3 +67,20 @@ export const editComment = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, 'Comentario no encontrado'));
+    }
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(errorHandler(403, 'No tienes permisos para eliminar este comentario'));
+    }
+    await Comment.findByIdAndDelete(req.params.commentId);
+    res.status(200).json({message: 'El Comentario ha sido eliminado'});
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
