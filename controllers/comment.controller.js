eb2a79723a1654f9c645a18c63d296bc4ef321b1
bautistalexander <145127728+bautistalexander@users.noreply.github.com>
2024-04-20 10:45:09 -0400

    Se adicionó la ruta para mostrar todos los comentarios

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -78,7 +78,34 @@ export const deleteComment = async (req, res, next) => {
       return next(errorHandler(403, 'No tienes permisos para eliminar este comentario'));
     }
     await Comment.findByIdAndDelete(req.params.commentId);
-    res.status(200).json({message: 'El Comentario ha sido eliminado'});
+    res.status(200).json({ message: 'El Comentario ha sido eliminado' });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+export const getComments = async (req, res, next) => {
+  try {
+    if (!req.user.isAdmin) {
+      return next(errorHandler(403, 'No tienes permisos para ver todos los comentarios'));
+    }
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 0;
+    const sortDirection = req.query.sort === 'desc' ? -1 : 1;
+    const comments = await Comment.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+    const totalComments = await Comment.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+    const lastMonthComments = await Comment.countDocuments({ createdAt: { $gte: oneMonthAgo } });
+    res.status(200).json({
+      comments,
+      totalComments,
+      lastMonthComments
+    });
   } catch (error) {
     console.log(error);
     next(error);
